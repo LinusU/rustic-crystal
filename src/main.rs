d03fda691e716544e387619817a646d1a4ea2c83
Linus Unnebäck <linus@folkdatorn.se>
2025-08-17 17:17:14 +0200

    wip: glium 0.36

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,21 @@
-use glium::glutin::platform::run_return::EventLoopExtRunReturn;
 use rustic_crystal::cpu::Cpu;
 use rustic_crystal::KeypadEvent;
 use std::sync::mpsc::{self, Receiver, SyncSender};
 use std::sync::{atomic::AtomicU64, Arc};
 use std::thread;
-
-#[derive(Default)]
-struct RenderOptions {
-    pub linear_interpolation: bool,
-}
+use winit::platform::pump_events::{EventLoopExtPumpEvents, PumpStatus};
 
 #[cfg(target_os = "windows")]
-fn create_window_builder() -> glium::glutin::window::WindowBuilder {
-    use glium::glutin::platform::windows::WindowBuilderExtWindows;
-    glium::glutin::window::WindowBuilder::new()
+fn create_window_attributes() -> winit::window::WindowAttributes {
+    use winit::platform::windows::WindowAttributesExtWindows;
+    winit::window::Window::default_attributes()
         .with_drag_and_drop(false)
-        .with_inner_size(glium::glutin::dpi::LogicalSize::<u32>::from((
-            rustic_crystal::SCREEN_W as u32,
-            rustic_crystal::SCREEN_H as u32,
-        )))
         .with_title("Rustic Crystal")
 }
 
 #[cfg(not(target_os = "windows"))]
-fn create_window_builder() -> glium::glutin::window::WindowBuilder {
-    glium::glutin::window::WindowBuilder::new()
-        .with_inner_size(glium::glutin::dpi::LogicalSize::<u32>::from((
-            rustic_crystal::SCREEN_W as u32,
-            rustic_crystal::SCREEN_H as u32,
-        )))
-        .with_title("Rustic Crystal")
+fn create_window_attributes() -> winit::window::WindowAttributes {
+    winit::window::Window::default_attributes().with_title("Rustic Crystal")
 }
 
 fn main() -> Result<(), &'static str> {
@@ -37,17 +23,17 @@ fn main() -> Result<(), &'static str> {
 
     let scale = 4;
 
-    let render_delay = Arc::new(AtomicU64::new(16_743));
+    let render_delay = Arc::new(AtomicU64::new(16));
 
     let (sender1, receiver1) = mpsc::channel();
     let (sender2, receiver2) = mpsc::sync_channel(1);
 
-    let mut eventloop = glium::glutin::event_loop::EventLoop::new();
-    let window_builder = create_window_builder();
-    let context_builder = glium::glutin::ContextBuilder::new();
-    let display =
-        glium::backend::glutin::Display::new(window_builder, context_builder, &eventloop).unwrap();
-    set_window_size(display.gl_window().window(), scale);
+    let mut event_loop = winit::event_loop::EventLoop::new().unwrap();
+    let window_attributes = create_window_attributes();
+    let (window, display) = glium::backend::glutin::SimpleWindowBuilder::new()
+        .set_window_builder(window_attributes)
+        .build(&event_loop);
+    set_window_size(&window, scale);
 
     let mut texture = glium::texture::texture2d::Texture2d::empty_with_format(
         &display,
@@ -58,137 +44,112 @@ fn main() -> Result<(), &'static str> {
     )
     .unwrap();
 
-    let mut renderoptions = <RenderOptions as Default>::default();
-
     let cputhread = thread::spawn(move || run_game(sender2, receiver1));
-    let periodic = timer_periodic(render_delay.clone());
+    // let periodic = timer_periodic(render_delay.clone());
 
-    eventloop.run_return(move |ev, _evtarget, controlflow| {
-        use glium::glutin::event::ElementState::{Pressed, Released};
-        use glium::glutin::event::VirtualKeyCode;
-        use glium::glutin::event::{Event, KeyboardInput, WindowEvent};
+    event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
 
-        let mut stop = false;
-        match ev {
-            Event::WindowEvent { event, .. } => match event {
-                WindowEvent::CloseRequested => stop = true,
-                WindowEvent::KeyboardInput { input, .. } => match input {
-                    KeyboardInput {
-                        state: Pressed,
-                        virtual_keycode: Some(VirtualKeyCode::Key1),
-                        ..
-                    } => render_delay.store(16_743, std::sync::atomic::Ordering::Relaxed), // 59.7 fps
-                    KeyboardInput {
-                        state: Pressed,
-                        virtual_keycode: Some(VirtualKeyCode::Key2),
-                        ..
-                    } => render_delay.store(10_000, std::sync::atomic::Ordering::Relaxed), // 100 fps
-                    KeyboardInput {
-                        state: Pressed,
-                        virtual_keycode: Some(VirtualKeyCode::Key3),
-                        ..
-                    } => render_delay.store(8_333, std::sync::atomic::Ordering::Relaxed), // 120 fps
-                    KeyboardInput {
-                        state: Pressed,
-                        virtual_keycode: Some(VirtualKeyCode::Key4),
-                        ..
-                    } => render_delay.store(5_000, std::sync::atomic::Ordering::Relaxed), // 200 fps
-                    KeyboardInput {
-                        state: Pressed,
-                        virtual_keycode: Some(VirtualKeyCode::Key5),
-                        ..
-                    } => render_delay.store(4_166, std::sync::atomic::Ordering::Relaxed), // 240 fps
-                    KeyboardInput {
-                        state: Pressed,
-                        virtual_keycode: Some(VirtualKeyCode::Key6),
-                        ..
-                    } => render_delay.store(2_500, std::sync::atomic::Ordering::Relaxed), // 400 fps
-                    KeyboardInput {
-                        state: Pressed,
-                        virtual_keycode: Some(VirtualKeyCode::Key7),
-                        ..
-                    } => render_delay.store(1_000, std::sync::atomic::Ordering::Relaxed), // 1000 fps
-                    KeyboardInput {
-                        state: Pressed,
-                        virtual_keycode: Some(VirtualKeyCode::T),
-                        ..
+    loop {
+        let timeout = Some(std::time::Duration::ZERO);
+        let status = event_loop.pump_events(timeout, |ev, elwt| {
+            use winit::event::ElementState::{Pressed, Released};
+            use winit::event::{Event, WindowEvent};
+            use winit::keyboard::Key;
+
+            if let Event::WindowEvent { event, .. } = ev {
+                match event {
+                    WindowEvent::CloseRequested => elwt.exit(),
+                    WindowEvent::KeyboardInput {
+                        event: keyevent, ..
                     } => {
-                        renderoptions.linear_interpolation = !renderoptions.linear_interpolation;
-                    }
-                    KeyboardInput {
-                        state: Pressed,
-                        virtual_keycode: Some(glutinkey),
-                        ..
-                    } => {
-                        if let Some(key) = glutin_to_keypad(glutinkey) {
-                            let _ = sender1.send(KeypadEvent::Down(key));
-                        }
-                    }
-                    KeyboardInput {
-                        state: Released,
-                        virtual_keycode: Some(glutinkey),
-                        ..
-                    } => {
-                        if let Some(key) = glutin_to_keypad(glutinkey) {
-                            let _ = sender1.send(KeypadEvent::Up(key));
+                        eprintln!("{:?} {:?}", keyevent.state, keyevent.logical_key);
+                        match (keyevent.state, keyevent.logical_key.as_ref()) {
+                            (Pressed, Key::Character("1")) => {
+                                // 59.7 fps
+                                render_delay.store(16_743, std::sync::atomic::Ordering::Relaxed)
+                            }
+                            (Pressed, Key::Character("2")) => {
+                                // 100 fps
+                                render_delay.store(10_000, std::sync::atomic::Ordering::Relaxed)
+                            }
+                            (Pressed, Key::Character("3")) => {
+                                // 120 fps
+                                render_delay.store(8_333, std::sync::atomic::Ordering::Relaxed)
+                            }
+                            (Pressed, Key::Character("4")) => {
+                                // 200 fps
+                                render_delay.store(5_000, std::sync::atomic::Ordering::Relaxed)
+                            }
+                            (Pressed, Key::Character("5")) => {
+                                // 240 fps
+                                render_delay.store(4_166, std::sync::atomic::Ordering::Relaxed)
+                            }
+                            (Pressed, Key::Character("6")) => {
+                                // 400 fps
+                                render_delay.store(2_500, std::sync::atomic::Ordering::Relaxed)
+                            }
+                            (Pressed, Key::Character("7")) => {
+                                // 1000 fps
+                                render_delay.store(1_000, std::sync::atomic::Ordering::Relaxed)
+                            }
+                            (Pressed, winitkey) => {
+                                if let Some(key) = winit_to_keypad(winitkey) {
+                                    let _ = sender1.send(KeypadEvent::Down(key));
+                                }
+                            }
+                            (Released, winitkey) => {
+                                if let Some(key) = winit_to_keypad(winitkey) {
+                                    let _ = sender1.send(KeypadEvent::Up(key));
+                                }
+                            }
                         }
                     }
                     _ => (),
-                },
-                _ => (),
-            },
-            Event::MainEventsCleared => {
-                periodic.recv().unwrap();
-
-                match receiver2.recv() {
-                    Ok(data) => {
-                        recalculate_screen(&display, &mut texture, &data, &renderoptions);
-                    }
-                    Err(..) => stop = true, // Remote end has hung-up
                 }
             }
-            _ => (),
+        });
+
+        if let PumpStatus::Exit(_) = status {
+            break;
         }
-        if stop {
-            *controlflow = glium::glutin::event_loop::ControlFlow::Exit;
+
+        // periodic.recv().unwrap();
+        // while periodic.try_recv().is_ok() {}
+
+        match receiver2.recv() {
+            Ok(data) => recalculate_screen(&display, &mut texture, &data),
+            Err(..) => break, // Remote end has hung-up
         }
-    });
+    }
 
     let _ = cputhread.join();
 
     Ok(())
 }
 
-fn glutin_to_keypad(
-    key: glium::glutin::event::VirtualKeyCode,
-) -> Option<rustic_crystal::KeypadKey> {
-    use glium::glutin::event::VirtualKeyCode;
+fn winit_to_keypad(key: winit::keyboard::Key<&str>) -> Option<rustic_crystal::KeypadKey> {
+    use winit::keyboard::{Key, NamedKey};
     match key {
-        VirtualKeyCode::Z => Some(rustic_crystal::KeypadKey::A),
-        VirtualKeyCode::X => Some(rustic_crystal::KeypadKey::B),
-        VirtualKeyCode::Up => Some(rustic_crystal::KeypadKey::Up),
-        VirtualKeyCode::Down => Some(rustic_crystal::KeypadKey::Down),
-        VirtualKeyCode::Left => Some(rustic_crystal::KeypadKey::Left),
-        VirtualKeyCode::Right => Some(rustic_crystal::KeypadKey::Right),
-        VirtualKeyCode::Space => Some(rustic_crystal::KeypadKey::Select),
-        VirtualKeyCode::Return => Some(rustic_crystal::KeypadKey::Start),
+        Key::Character("z" | "Z") => Some(rustic_crystal::KeypadKey::A),
+        Key::Character("x" | "X") => Some(rustic_crystal::KeypadKey::B),
+        Key::Named(NamedKey::ArrowUp) => Some(rustic_crystal::KeypadKey::Up),
+        Key::Named(NamedKey::ArrowDown) => Some(rustic_crystal::KeypadKey::Down),
+        Key::Named(NamedKey::ArrowLeft) => Some(rustic_crystal::KeypadKey::Left),
+        Key::Named(NamedKey::ArrowRight) => Some(rustic_crystal::KeypadKey::Right),
+        Key::Named(NamedKey::Space) => Some(rustic_crystal::KeypadKey::Select),
+        Key::Named(NamedKey::Enter) => Some(rustic_crystal::KeypadKey::Start),
         _ => None,
     }
 }
 
-fn recalculate_screen(
-    display: &glium::Display,
+fn recalculate_screen<
+    T: glium::glutin::surface::SurfaceTypeTrait + glium::glutin::surface::ResizeableSurface + 'static,
+>(
+    display: &glium::Display<T>,
     texture: &mut glium::texture::texture2d::Texture2d,
     datavec: &[u8],
-    renderoptions: &RenderOptions,
 ) {
     use glium::Surface;
-
-    let interpolation_type = if renderoptions.linear_interpolation {
-        glium::uniforms::MagnifySamplerFilter::Linear
-    } else {
-        glium::uniforms::MagnifySamplerFilter::Nearest
-    };
 
     let rawimage2d = glium::texture::RawImage2d {
         data: std::borrow::Cow::Borrowed(datavec),
@@ -217,7 +178,7 @@ fn recalculate_screen(
             width: target_w as i32,
             height: -(target_h as i32),
         },
-        interpolation_type,
+        glium::uniforms::MagnifySamplerFilter::Nearest,
     );
     target.finish().unwrap();
 }
@@ -232,6 +193,7 @@ fn timer_periodic(delay: Arc<AtomicU64>) -> Receiver<()> {
     let (tx, rx) = std::sync::mpsc::sync_channel(1);
     std::thread::spawn(move || loop {
         let micros = delay.load(std::sync::atomic::Ordering::Relaxed);
+        // eprintln!("Waiting for {micros} micros");
         std::thread::sleep(std::time::Duration::from_micros(micros));
         if tx.send(()).is_err() {
             break;
@@ -240,16 +202,9 @@ fn timer_periodic(delay: Arc<AtomicU64>) -> Receiver<()> {
     rx
 }
 
-fn set_window_size(window: &glium::glutin::window::Window, scale: u32) {
-    use glium::glutin::dpi::{LogicalSize, PhysicalSize};
-
-    let dpi = window.scale_factor();
-
-    let physical_size = PhysicalSize::<u32>::from((
+fn set_window_size(window: &winit::window::Window, scale: u32) {
+    let _ = window.request_inner_size(winit::dpi::LogicalSize::<u32>::from((
         rustic_crystal::SCREEN_W as u32 * scale,
         rustic_crystal::SCREEN_H as u32 * scale,
-    ));
-    let logical_size = LogicalSize::<u32>::from_physical(physical_size, dpi);
-
-    window.set_inner_size(logical_size);
+    )));
 }
